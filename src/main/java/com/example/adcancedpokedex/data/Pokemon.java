2eb1e4605b6134f48e5a159d835f24249940469c
JustWantCookies <lucas.mager1607@gmail.com>
2023-11-27 20:48:20 +0100

    add Pokemon Cell Item and List

--- src/main/java/com/example/adcancedpokedex/data/Pokemon.java
+++ src/main/java/com/example/adcancedpokedex/data/Pokemon.java
@@ -4,6 +4,21 @@ public class Pokemon {
     //TODO fill data here
     private String name;
 
+    private int level;
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public Pokemon (String name, int level){
+        this.name = name;
+        this.level = level;
+    }
+
     public String getName() {
         return name;
     }
--- src/main/java/com/example/advancedpokedex/ui/AbstractPokedex.java
+++ src/main/java/com/example/advancedpokedex/ui/AbstractPokedex.java
@@ -1,13 +1,13 @@
 package com.example.advancedpokedex.ui;
 
 import com.example.adcancedpokedex.data.Pokemon;
+import com.example.advancedpokedex.ui.internal.PokemonListCell;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
 public abstract class AbstractPokedex extends Application {
@@ -25,10 +25,21 @@ public abstract class AbstractPokedex extends Application {
 
         pane.setTop(searchField);
 
-        //TODO cell factory for Pokemon Item -> https://stackoverflow.com/questions/36657299/how-can-i-populate-a-listview-in-javafx-using-custom-objects
-        //TODO PokemonView Item
+        //TODO Remove
+        addTestData();
+
+        ListView<Pokemon> listViewPokemon = new ListView<>(pokemonList);
+        listViewPokemon.setCellFactory(param -> new PokemonListCell());
+
+        pane.setCenter(listViewPokemon);
 
         return pane;
+    }
+
+    private void addTestData(){
+        pokemonList.add(new Pokemon("Glumanda", 1));
+        pokemonList.add(new Pokemon("Arceus", 100));
+        pokemonList.add(new Pokemon("Zekrom", 50));
     }
 
 }
