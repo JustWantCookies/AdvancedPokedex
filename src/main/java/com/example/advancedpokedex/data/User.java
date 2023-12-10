af94ef96d2a3e82f0bd9ebec9eff63d8ddd07b7d
Torsten Egert <torsten.egert@martinek.or.at>
2023-12-10 19:23:30 +0100

    make class user threadsafe

--- src/main/java/com/example/advancedpokedex/data/User.java
+++ src/main/java/com/example/advancedpokedex/data/User.java
@@ -1,5 +1,43 @@
 package com.example.advancedpokedex.data;
 
+//ToDo: Write JavaDoc
+
 public class User
 {
+    private final int uid; //muss auto-generated sein, denn darf nur einmal vorkommen
+    private String uname; //Username plain-text
+
+    private String passwd; //Passwort hashed
+
+    public User(int uid,String uname, String passwd)
+    {
+        this.uid=uid;
+        this.uname = uname;
+        this.passwd = passwd;
+    }
+
+    public synchronized int getUid()
+    {
+        return uid;
+    }
+
+    public synchronized String getUname()
+    {
+        return uname;
+    }
+
+    public synchronized void setUname(String uname)
+    {
+        this.uname = uname;
+    }
+
+    public synchronized String getPasswd()
+    {
+        return passwd;
+    }
+
+    public synchronized void setPasswd(String passwd)
+    {
+        this.passwd = passwd;
+    }
 }
